7cadc70a3c25845814e56970adc014651c2f38a0
Jake Foster <fosterjakea@outlook.com>
2020-12-22 20:11:03 +1100

    Readability improvements

--- endless-runner/MainWindow.xaml.cs
+++ endless-runner/MainWindow.xaml.cs
@@ -13,51 +13,39 @@ namespace endless_runner
     /// </summary>
     public partial class MainWindow : Window
     {
-        // Instance of dispatcher timer.
+        // Dispatcher.
         private readonly DispatcherTimer gameTimer = new DispatcherTimer();
 
-        // Rect class instances called player hit box, ground hit box, obstacle hit box.
+        // Hit boxes for all objects.
         private Rect playerHitBox;
         private Rect groundHitBox;
         private Rect obstacleHitBox;
-
-        // Jumping status, default is false.
-        private bool jumping;
-
-        // Force integer, default is 20.
-        private int force = 20;
-
-        // Speed integer, default is 5.
-        private int speed = 5;
-
-        // Random class instance.
-        private readonly Random rand = new Random();
-
-        // Game over status, default is false.
-        private bool gameover;
-
-        // Used to swap sprites for the player.
-        private double spriteInt;
 
         // Sprite variable, used for swapping sprites of the player.
         private readonly ImageBrush playerSprite = new ImageBrush();
         private readonly ImageBrush backgroundSprite = new ImageBrush();
         private readonly ImageBrush obstacleSprite = new ImageBrush();
 
-        // Array to change the obstacle position on screen.
-        // Random element is selected for the height after the first one.
-        private readonly int[] obstaclePosition = { 275, 280, 285, 290, 290 };
+        // Randomly generate height and distance of obstacles.
+        private readonly Random rand = new Random();
 
-        // Score, default is 0.
+        // Default variables.
+        private bool gameover;
+        private bool jumping;
+        private double spriteInt;
+        private int backgroundSpeed;
+        private int force;
+        private int nextObstacle;
+        private int obstacleMovement;
         private int score;
-
-        // Speed at which the game moves. After every jump, speed increases.
-        // Default speeds are 3, 12.
-        private int backgroundSpeed = 3;
-        private int playerSpeed = 12;
-
-        // Gap between the last and next obstacle.
-        private int nextObstacle = 950;
+        private int speed;
+        private readonly int backgroundWidth = 1262;
+        private readonly int obstacleMaxHeight = 290;
+        private readonly int obstacleMinHeight = 275;
+        private readonly int obstacleStartX = 280;
+        private readonly int obstacleStartY = 950;
+        private readonly int playerStartX = 110;
+        private readonly int playerStartY = 140;
 
         public MainWindow()
         {
@@ -70,8 +58,7 @@ namespace endless_runner
             // Set game timer interval to 20 ms.
             gameTimer.Interval = TimeSpan.FromMilliseconds(20);
 
-            // Set back sprite image.
-            // 'pack://' - Current app package.
+            // Set background sprite image.
             backgroundSprite.ImageSource = new BitmapImage(new Uri(Paths.BackgroundImage, UriKind.Relative));
             Background1.Fill = backgroundSprite;
             Background2.Fill = backgroundSprite;
@@ -100,26 +87,29 @@ namespace endless_runner
             if (e.Key == Key.Enter && gameover)
             {
                 backgroundSpeed = 3;
-                playerSpeed = 12;
+                obstacleMovement = 12;
                 nextObstacle = 950;
 
                 StartGame();
             }
         }
 
+        /// <summary>
+        /// Starts the game.
+        /// </summary>
         private void StartGame()
         {
             // Background location.
             Canvas.SetLeft(Background1, 0);
-            Canvas.SetLeft(Background2, 1262);
+            Canvas.SetLeft(Background2, backgroundWidth);
 
             // Player starting x, y coordinate.
-            Canvas.SetLeft(Player, 110);
-            Canvas.SetTop(Player, 140);
+            Canvas.SetLeft(Player, playerStartX);
+            Canvas.SetTop(Player, playerStartY);
 
             // Obstacle starting x, y coordinate.
-            Canvas.SetLeft(Obstacle, 950);
-            Canvas.SetTop(Obstacle, 280);
+            Canvas.SetLeft(Obstacle, obstacleStartY);
+            Canvas.SetTop(Obstacle, obstacleStartX);
 
             // Set sprite for player.
             RunSprite(1);
@@ -129,10 +119,15 @@ namespace endless_runner
             Obstacle.Fill = obstacleSprite;
 
             // Starting values.
-            jumping = false;
+            backgroundSpeed = 3;
+            force = 5;
             gameover = false;
+            jumping = false;
+            nextObstacle = 950;
+            obstacleMovement = 12;
             score = 0;
-            ScoreText.Content = "Score: " + score;
+            speed = 5;
+            ScoreText.Content = $"Score: {score}";
 
             // Start the game timer.
             gameTimer.Start();
@@ -185,20 +180,25 @@ namespace endless_runner
             Player.Fill = playerSprite;
         }
 
+        /// <summary>
+        /// Main logic of the game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GameEngine(object sender, EventArgs e)
         {
             // Move the player character down using the speed integer.
             Canvas.SetTop(Player, Canvas.GetTop(Player) + speed);
 
-            // Move the background 3 pixels to the left with each tick.
+            // Move the background pixels to the left with each tick.
             Canvas.SetLeft(Background1, Canvas.GetLeft(Background1) - backgroundSpeed);
             Canvas.SetLeft(Background2, Canvas.GetLeft(Background2) - backgroundSpeed);
 
-            // Move obstacle rectangle to the left 12 pixels per tick.
-            Canvas.SetLeft(Obstacle, Canvas.GetLeft(Obstacle) - playerSpeed);
+            // Move obstacle rectangle to the left pixels per tick.
+            Canvas.SetLeft(Obstacle, Canvas.GetLeft(Obstacle) - obstacleMovement);
 
             // Link the score text label to the score integer.
-            ScoreText.Content = "Score: " + score;
+            ScoreText.Content = $"Score: {score}";
 
             // Assign the player hit box to the player, ground hit box to the ground, rectangle and obstacle hit box to the obstacle rectangle.
             playerHitBox = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
@@ -208,10 +208,10 @@ namespace endless_runner
             // If the player hits the ground.
             if (playerHitBox.IntersectsWith(groundHitBox))
             {
-                speed = 0;
                 Canvas.SetTop(Player, Canvas.GetTop(Ground) - Player.Height);
+                speed = 0;
+                spriteInt++;
                 jumping = false;
-                spriteInt += 1;
 
                 // Reset the sprite if it goes above 8.
                 if (spriteInt > 8)
@@ -223,14 +223,14 @@ namespace endless_runner
                 RunSprite(spriteInt);
             }
 
-            // If the player hits the obstacle.
+            // Intersection with the box.
             if (playerHitBox.IntersectsWith(obstacleHitBox))
             {
                 gameover = true;
                 gameTimer.Stop();
             }
 
-            // If jumping.
+            // Player jumping.
             if (jumping)
             {
                 speed = -9;
@@ -252,14 +252,14 @@ namespace endless_runner
 
             // If the first background goes below a certain point, position 
             // the first background behind the second background.
-            if (Canvas.GetLeft(Background1) < -1262)
+            if (Canvas.GetLeft(Background1) < -backgroundWidth)
             {
                 Canvas.SetLeft(Background1, Canvas.GetLeft(Background2) + Background2.Width);
             }
 
             // If the second background goes below a certain point, position 
             // the second background behind the first background.
-            if (Canvas.GetLeft(Background2) < -1262)
+            if (Canvas.GetLeft(Background2) < -backgroundWidth)
             {
                 Canvas.SetLeft(Background2, Canvas.GetLeft(Background1) + Background1.Width);
             }
@@ -270,12 +270,14 @@ namespace endless_runner
             // around the screen. Also adds 1 to the score tally.
             if (Canvas.GetLeft(Obstacle) < -50)
             {
+                // Distance to next obstacle: Previous obstacle plus random number between 1-100 pixels.
+                // Height of next obstacle: Random number between the min and max height in pixels.
                 Canvas.SetLeft(Obstacle, nextObstacle + rand.Next(1, 100));
-                Canvas.SetTop(Obstacle, obstaclePosition[rand.Next(0, obstaclePosition.Length)]);
+                Canvas.SetTop(Obstacle, rand.Next(obstacleMinHeight, obstacleMaxHeight));
 
                 score++;
                 backgroundSpeed++;
-                playerSpeed++;
+                obstacleMovement++;
             }
 
             // If gameover, draw a yellow border around the obstacle. Draw a red border around
